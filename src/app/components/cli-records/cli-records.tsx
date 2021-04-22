f1ec7d6d84fe02c316f090eda088fccebc28e780
sanjeevE1996 <sanjeev.dhillon@esure.com>
2021-04-22 14:30:23 +0100

    feat: cli records component now functional

--- src/app/components/cli-records/cli-records.tsx
+++ src/app/components/cli-records/cli-records.tsx
@@ -1,51 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { IComps } from "../../intefaces";
 import "./cli-records.scss";
 import * as Sentry from "@sentry/react";
 
-export class CliRecords extends React.Component {
-  constructor(props: {}) {
-    super(props);
+export const CliRecords: React.FC<IComps> = () => {
+  const [filterComp, setFilterComp] = useState({
+    filterComp: false,
+  } as IComps);
 
-    this.state = {
-      filterComp: false,
-    } as IComps;
-  }
+  console.log(filterComp);
 
-  throwknownError = () => {
-    throw new Error("this is a test error");
+  const throwknownError = () => {
+    throw new Error("Sentry test error");
   };
 
-  render() {
-    return (
-      <Sentry.ErrorBoundary fallback={"An error has occurred"}>
-        <div>
-          <section>
-            <header className="records-header">CLI Records</header>
-            <div className="records-canvas">
-              <div className="records-container">
-                <div className="records-card">
-                  <div className="btn-container">
-                    <button
-                      className="filter-btn"
-                      onClick={() => this.setState({ filterComp: true })}
-                    >
-                      Show Filters
-                    </button>
-                    <button className="number-btn">Add Number</button>
-                  </div>
-                  <div>CLI Records</div>
-                  <div>Notes </div>
-                  <button onClick={this.throwknownError}>
-                    Break the world
+  return (
+    <Sentry.ErrorBoundary fallback={"An error has occurred"}>
+      <div>
+        <section>
+          <header className="records-header">CLI Records</header>
+          <div className="records-canvas">
+            <div className="records-container">
+              <div className="records-card">
+                <div className="btn-container">
+                  <button
+                    className="filter-btn"
+                    onClick={() => setFilterComp({ filterComp: true })}
+                  >
+                    Show Filters
                   </button>
-                  ;
+                  <button className="number-btn">Add Number</button>
                 </div>
+                <div>CLI Records</div>
+                <div>Notes </div>
+                <button onClick={throwknownError}>Break the world</button>;
               </div>
             </div>
-          </section>
-        </div>
-      </Sentry.ErrorBoundary>
-    );
-  }
-}
+          </div>
+        </section>
+      </div>
+    </Sentry.ErrorBoundary>
+  );
+};
